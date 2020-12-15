0846bb005f72ca57141d2be14bf5dbcca83435db
Dorota <d.a.lewinska@gmail.com>
2020-12-15 11:43:08 +0000

    Router installed and multipage structure established.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 //import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, 
+  NavLink
+} from "react-router-dom";
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
 import Sidebar from './components/Sidebar/Sidebar.js';
 import PageContentLanding from './components/PageContentLanding/PageContentLanding.js';
 
+
 function App() {
   return (
+  <Router>  
     <div className="App">
     <Header />
 
   <main role="main" className="flex-shrink-0">
     <div className="container">
       <div className="row">
-        <PageContentLanding />
-        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <PageContentLanding />
+            <Sidebar />
+          </Route>
+          <Route path="/about">
+            <div className="col-sm-12 col-md-8 col-lg-9 col-xl-9">
+              <p>Lorem ispum dolor.</p>
+            </div>
+            <Sidebar />
+          </Route>
+          <Route path="/map">
+            <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+              <p>Map goes here.</p>
+            </div>
+          </Route>
+        </Switch>  
       </div>
 
     </div>
@@ -22,6 +45,7 @@ function App() {
 
     <Footer />
     </div>
+    </Router>  
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,28 @@
-
+import './Header.css';
+import {
+  BrowserRouter as Router,
+  NavLink
+} from "react-router-dom";
 function Header() {
     return (
+  
         <header>
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
           <div className="container">
-          <a className="navbar-brand" href="#">Let's Recycle</a>
+          <a className="navbar-brand" href="/">Let's Recycle</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse " id="navbarCollapse">
             <ul className="navbar-nav ml-auto ">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <li className="nav-item">
+                <NavLink to="/" activeClassName="active">Home</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About Us</a>
+                <NavLink to="/about" activeClassName="active">About</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Manage Your Collection</a>
+                <NavLink to="/map" activeClassName="active">Map</NavLink>
               </li>
             </ul>
           </div>
